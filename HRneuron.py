acf8cbe0b0e323214c2ad8435186e8fa8bfed7c5
Kailey Hua <59590797+kaileyhh@users.noreply.github.com>
2022-07-26 14:37:16 -0400

    hr neuron update :D

--- HRneuron.py
+++ HRneuron.py
@@ -1,10 +1,81 @@
+
+
 # import imports
 from imports import *
 
 c = constants()
 
+# x(t): membrane potential
+# y(t): sodium, potassium transport rate (spiking variable)
+# z(t): adaptation current (decreases firing rate after spike)
+
+# I: current, control parameter
+# a, b, c, d: modeling of fast ion channels
+# r: modeling slow ion channel
+# usually s = 4 and xR = -8/5
+
 class HRneuron:
     def __init__(self, id):
         self.id = id
+        # self.s = c.FIXED_S
+        # self.xR = c.FIXED_XR
+        # self.dt = c.TIME_INCREMENT
+
+        # self.a = c.FIXED_A
+        # self.b = c.FIXED_B
+        # self.c = c.FIXED_C
+        # self.d = c.FIXED_D
+        # self.r = c.FIXED_R
+
+        self.a = c.a
+        self.b = c.b
+        self.c = c.c
+        self.d = c.c
+        self.r = c.r
+        self.s = c.s
+        self.xR = c.xR
+        self.current = c.I
+        self.scale = c.scale
+
+        # self.I = current
+        # self.x = np.zeros(c.ARR_SIZE)
+        # self.x[0] = -1.6
+        # self.y = np.zeros(c.ARR_SIZE)
+        # self.y[0] = 4.0
+        # self.z = np.zeros(c.ARR_SIZE)
+        # self.z[0] = 2.75
+
+        self.xarr = np.zeros(c.iterations)
+        self.yarr = np.zeros(c.iterations)
+        self.zarr = np.zeros(c.iterations)
+
+        self.x = -1.6
+        self.y = 4.0
+        self.z = 2.75
+    
+    def phi_x(self, x):
+        return (-1 * self.a * x**3 + self.b * x**2)
+    
+    def trident_x(self, x):
+        return (self.c - self.d * x**2)
+
+    def calculate_x(self, time):
+        dx = self.y + (self.b * self.x * self.x) - (self.a * self.x * self.x * self.x) - self.z + self.current
+        self.x += self.scale * dx
+        self.xarr[time] = self.x
+        # self.x[time] = self.x[time-1] + (self.y[time-1] + self.phi_x(self.x[time-1]) - self.z[time-1] + self.I[time]) * self.dt 
+    
+    def calculate_y(self, time):
+        dy = self.c - (5 * self.x * self.x) - self.y
+        self.y += self.scale * dy
+        self.yarr[time] = self.y
+        # self.y[time] = self.y[time-1] + (self.trident_x(self.x[time-1]) - self.y[time-1]) * self.dt
+    
+    def calculate_z(self, time):
+        dz = self.r * (self.s * (self.x - self.xR) - self.z)
+        self.z += self.scale * dz
+        self.zarr[time] = self.z
+        # self.z[time] = self.z[time-1] + (self.r * (self.s * (self.x[time-1] - self.xR) - self.z[time-1])) * self.dt
+
     
     # insert formulas and things here
--- imports.py
+++ imports.py
@@ -5,3 +5,31 @@ import matplotlib.pyplot as plt
 class constants:
     def __init__(self):
         self.EXAMPLE_CONSTANT = 0
+        self.TIME_INCREMENT = 0.0001
+        self.OVERALL_TIME = 5
+
+        self.ARR_SIZE = int(self.OVERALL_TIME / self.TIME_INCREMENT)
+
+        # HR NEURON, from wikipedia :>
+
+        # self.FIXED_S = 4.0
+        # self.FIXED_XR = -8/5
+        # self.FIXED_A = 1.0
+        # self.FIXED_B = 3.0
+        # self.FIXED_C = 1.0
+        # self.FIXED_D = 5.0
+        # self.FIXED_R = 10e-3
+
+        
+        # source: https://jamesmccaffrey.wordpress.com/2020/01/27/hindmarsh-rose-model-simulation-using-c-or-python/
+        self.a = 1.0
+        self.b = 3.0
+        self.c = 1.0
+        self.d = 5.0
+        self.r = 0.005
+        self.s = 4.0
+        self.xR = -1.6
+        self.scale = 0.001
+        self.ms = 200
+        self.iterations = self.ms * 1000
+        self.I = 5
