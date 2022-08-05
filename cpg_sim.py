539cd09e24f45e7af62db93eb11e108c283b6511
Kailey Hua <59590797+kaileyhh@users.noreply.github.com>
2022-08-05 14:58:07 -0400

    cpg stimulus update

--- cpg_sim.py
+++ cpg_sim.py
@@ -9,6 +9,10 @@ pre_botc = HRneuronseg(1)
 pico = HRneuronseg(2)
 rtn = HRneuronseg(3)
 
+pre_botc.set_current(5.0)
+pico.set_current(4.0)
+rtn.set_current(4.0)
+
 # pico.set_current(2.5)
 # rtn.set_current(2.0)
 
@@ -21,9 +25,9 @@ cpg.attach_neurons(rtn, pico)
 cpg.attach_neurons(pre_botc, rtn)
 cpg.attach_neurons(rtn, pre_botc)
 
-cpg.create_weight_mat(pre_botc, [5.2, 1.7])
-cpg.create_weight_mat(pico, [5.0, 2.5])
-cpg.create_weight_mat(rtn, [2.1, 2.0])
+cpg.create_weight_mat(pre_botc, [50.2, 40.7])
+cpg.create_weight_mat(pico, [50.0, 40.5])
+cpg.create_weight_mat(rtn, [40.1, 40.0])
 
 for i in range(c.iterations):
     cpg.calculate_weight_all(i)
@@ -38,28 +42,37 @@ ax2 = fig.add_subplot(222)
 ax3 = fig.add_subplot(223)
 ax4 = fig.add_subplot(224)
 
-ax1.plot(pre_botc.time_vec, pre_botc.xarr, linewidth=0.5, color="red")
-ax2.plot(pico.time_vec, pico.xarr, linewidth=0.5, color="green")
-ax3.plot(rtn.time_vec, rtn.xarr, linewidth=0.5, color="blue")
+ax1.plot(pre_botc.time_vec, pre_botc.xarr, linewidth=0.5, color=c.l_blue)
+ax2.plot(pico.time_vec, pico.xarr, linewidth=0.5, color=c.l_yellow)
+ax3.plot(rtn.time_vec, rtn.xarr, linewidth=0.5, color="black")
 
-ax4.plot(pre_botc.time_vec, pre_botc.xarr, linewidth=0.5, color="red")
-ax4.plot(pico.time_vec, pico.xarr, linewidth=0.5, color="green")
-ax4.plot(rtn.time_vec, rtn.xarr, linewidth=0.5, color="blue")
+ax4.plot(pre_botc.time_vec, pre_botc.xarr, linewidth=0.5, color=c.l_blue)
+ax4.plot(pico.time_vec, pico.xarr, linewidth=0.5, color=c.l_yellow)
+ax4.plot(rtn.time_vec, rtn.xarr, linewidth=0.5, color="black")
 
-ax1.title.set_text("Pre-BotC (red, I = " + str(pre_botc.current) + " ), x0 = " + str(pre_botc.xR))
-ax1.set_xlabel("time (ms)")
-ax1.set_ylabel("au")
-ax2.title.set_text("PiCo (green, I = " + str(pico.current) + " ), x0 = " + str(pico.xR))
-ax2.set_xlabel("time (ms)")
-ax2.set_ylabel("au")
-ax3.title.set_text("RTN/pFRG (green, I = " + str(rtn.current) + " ), x0 = " + str(rtn.xR))
-ax3.set_xlabel("time (ms)")
-ax3.set_ylabel("au")
-ax4.title.set_text("All Neurons Together")
-ax4.set_xlabel("time (ms)")
-ax4.set_ylabel("au")
+ax1.set_title("Pre-BotC, [PiCo = 50.2, RTN/pFRG = 40.7]", fontsize=8)
+ax1.set_xlabel("Time (ms)", fontsize=8)
+ax1.set_ylabel("Membrane Potential (au)", fontsize=8)
+ax2.set_title("PiCo, [Pre-BotC = 50.0, RTN/pFRG = 40.5]", fontsize=8)
+ax2.set_xlabel("Time (ms)", fontsize=8)
+ax2.set_ylabel("au", fontsize=8)
+ax3.set_title("RTN/pFRG, [Pre-Botc = 40.1, PiCo = 40.0]", fontsize=8)
+ax3.set_xlabel("Time (ms)", fontsize=8)
+ax3.set_ylabel("Membrane Potential (au)", fontsize=8)
+ax4.set_title("All Complexes Together", fontsize=8)
+ax4.set_xlabel("Time (ms)", fontsize=8)
+ax4.set_ylabel("Membrane Potential (au)", fontsize=8)
 
 # plt.rc('figure', titlesize=8) 
-fig.suptitle("CPG Neurons (I = " + str(c.I) + ")")
+fig.suptitle("CPG Neurons (Pre- BotC I = " + str(pre_botc.current) + ", others I = 4.0; baseline)")
+
+plt.subplots_adjust(left=0.1,
+                    bottom=0.1, 
+                    right=0.9, 
+                    top=0.9, 
+                    wspace=0.4, 
+                    hspace=0.4)
+
+plt.savefig("cpg.png", dpi=300)
 
 plt.show()
